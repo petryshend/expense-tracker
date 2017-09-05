e93189c27ba686d948e511ed4a61c60e71256462
petryshend <petryshend@gmail.com>
2017-09-05 17:00:32 +0300

    ass toastr notifications

--- web/js/script.js
+++ web/js/script.js
@@ -1,5 +1,4 @@
 $(function() {
-
     $('#new-expense-modal').on('shown.bs.modal', function () {
         $('#new-expense-title').focus()
     });
@@ -7,4 +6,20 @@ $(function() {
     $('#create-expense-btn').on('click', function() {
         $('#create-expense-form').submit();
     });
+
+    var $createExpenseForm = $('#create-expense-form');
+    $createExpenseForm.on('keyup', function(event) {
+        if (event.keyCode === 13) {
+            $(this).submit();
+        }
+    });
+    $createExpenseForm.on('submit', function() {
+        if ($('#new-expense-title').val() === '' || $('#new-expense-amount').val() === '') {
+            toastr.warning('You must enter title and amount');
+            return false;
+        } else {
+            return true;
+        }
+    })
+
 });
